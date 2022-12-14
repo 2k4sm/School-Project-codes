3362757a0904c702b125771f7de72df907703925
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2022-12-14 23:52:19 +0530

    table creation done.

--- cs_pro/last4/create.py
+++ cs_pro/last4/create.py
@@ -1,5 +1,20 @@
 import mariadb as md
 
-con = md.connect(host = 'localhost',user ='root',passwd = 'heaven',database = 'sm2k4')
+con = md.connect(host = 'localhost',user ='root',passwd = 'heaven')
 curs = con.cursor()
+
+try:
+    curs.execute(f"create database AISSCE2023XII")
+    print(f"Database created")
+    curs.execute(f"use AISSCE2023XII")
+    curs.execute(f"create table Book (BookNo int, BookName varchar(20),Price int)")
+    print("Table created.")
+except md.ProgrammingError:
+    print(f"Database already exists.")
+    try:
+        curs.execute(f"use AISSCE2023XII")
+        curs.execute(f"create table Book (BookNo int, BookName varchar(20),Price int)")
+        print("Table created.")
+    except md.OperationalError:
+        print(f"Table already exixts.")
 
