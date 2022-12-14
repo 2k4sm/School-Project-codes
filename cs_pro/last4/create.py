540081801dec6a2db479223cac7a94703569f23b
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2022-12-15 00:21:28 +0530

    reformatted

--- cs_pro/last4/create.py
+++ cs_pro/last4/create.py
@@ -6,15 +6,15 @@ curs = con.cursor()
 try:
     curs.execute(f"create database AISSCE2023XII")
     print(f"Database created")
-    curs.execute(f"use AISSCE2023XII")
-    curs.execute(f"create table Book (BookNo int, BookName varchar(20),Price int)")
+    curs.execute("use AISSCE2023XII")
+    curs.execute("create table Book (BookNo int, BookName varchar(20),Price int)")
     print("Table created.")
 except md.ProgrammingError:
-    print(f"Database already exists.")
+    print("Database already exists.")
     try:
-        curs.execute(f"use AISSCE2023XII")
-        curs.execute(f"create table Book (BookNo int, BookName varchar(20),Price int)")
+        curs.execute("use AISSCE2023XII")
+        curs.execute("create table Book (BookNo int, BookName varchar(20),Price int)")
         print("Table created.")
     except md.OperationalError:
-        print(f"Table already exixts.")
+        print("Table already exixts.")
 
